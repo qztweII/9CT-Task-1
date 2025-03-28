c2d8d53d6c4a7902192a6810995e6742a5795be6
Aixo235 <eli.bodhi.bishop@gmail.com>
2025-03-28 12:30:23 +1100

    theory

---
+++ Challenge_code.py
@@ -0,0 +1,38 @@
+a = "s"
+seeing_red = False
+seeing_yellow = False
+grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def find():
+    move in a pattern i havent decided yet or random
+def found():
+    if distance < 20:
+        while distance != 5:
+            go_forward
+        if distance == (4, 6) and colour == (71, 80):
+            seeing_yellow = True
+        elif distance == (4, 6) and colour == (46, 55):
+            seeing_red = True
+        else:
+            record_other()
+def():
+def():
+def():
+def():
+def():
+
+while a != 5:
+    while no_red and no_yellow:
+        find()
+        found()
+    pushred()
+    pushyellow()
