a7dc26700d6eb0c4838e8d99eca94a35921ff064
Aixo235 <eli.bodhi.bishop@gmail.com>
2025-05-02 13:30:26 +1000

    Update Challenge_code.py

--- Challenge_code.py
+++ Challenge_code.py
@@ -8,10 +8,17 @@ colour = PortX #replace with colour sensor
 def find():
     global seeY, seeR
     while distance > 200:
-        turn_right5 #
+        turn_left195
+        forward10
+        turn_right50 #
         wait800 #
+        for i in range(12):
+            if distance <200:
+                break
+            turn_right5
+            wait800
     while distance > 5:
-        go_forward #
+        forward3 #
     if colour == (75,84):
         seeY = True
         push_to_destination #
